a16eaba282204032f09c325199bb9be527a11f72
M-Farhan-R <32436840+VxyX@users.noreply.github.com>
2024-02-18 22:27:24 +0900

    updet dikit

---
+++ Assets/Scenes/SceneScript/Stages.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StagesScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Stage11Btn()
+    {
+        SceneManager.LoadScene("Stage11");
+    }
+    public void Stage12Btn()
+    {
+        
+    }
+    public void Stage13Btn()
+    {
+        
+    }
+    public void Stage14Btn()
+    {
+        
+    }
+    public void Stage15Btn()
+    {
+        
+    }
+    public void Stage16Btn()
+    {
+        
+    }
+}
